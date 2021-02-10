7b943c9720bbc5a59a691d29e48a2c445d8c0d41
Ervin Sabic <esabic@gearsite.net>
2021-02-09 21:22:52 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php 
+	// I'm including composer's generated autoload.php so I have access to any library I install.
 	require("vendor/autoload.php");	
+	// I'm telling twig where my templates are located. In my case, it's the 'myTemplates' folder. 
 	$loader = new \Twig\Loader\FilesystemLoader('myTemplates');
+	// I'm setting up the actual twig environment. 
 	$twig = new \Twig\Environment($loader);
-
+	// Just some data that I randomly came up with. Notice that it is a nested array that has a key "people" in it. 
 	$myData = ["people"=>["Jill", "Jan", "Bob", "Frank"]];
+	// This tells twig which file I want to load up. 
 	$template = $twig->load("class.html.twig");
-
+	// I pass my array of people to the render method to turn my curly brackets into actual data. 
 	echo $template->render($myData);
 ?>
